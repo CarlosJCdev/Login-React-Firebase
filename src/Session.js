1c4149d6cd862c0bdc6b66c7fc3679e237c7275d
CarlosJCdev <carlos4561059919@gmail.com>
2020-08-14 00:53:44 -0500

    Se cambio estylo de color y contenido de texto, se agregaron iconos

--- src/Session.js
+++ src/Session.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import '../src/Style/Session.css';
+import fb from '../src/icons/facebook.png';
+import go from '../src/icons/google.png';
+import git from '../src/icons/github.png';
 
 export class Session extends Component {
 
@@ -37,15 +40,15 @@ export class Session extends Component {
                     <form action="#">
                         <h1>Create Account</h1>
                         <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="#"  className="social"><img src={fb} className="icon" alt="Facebook"/></a>
+                            <a href="#"  className="social"><img src={go} className="icon" alt="Google"/></a>
+                            <a href="#"  className="social"><img src={git} className="icon" alt="Github"/></a>
                         </div>
                         <span>or use your email for registration</span>
                         <input type="text" placeholder="Name" />
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
+                        <button className="Up">Sign Up</button>
                     </form>
                 </div>
 
@@ -54,15 +57,15 @@ export class Session extends Component {
                     <form action="#">
                         <h1>Sign in</h1>
                         <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="#"  className="social"><img src={fb} className="icon" alt="Facebook"/></a>
+                            <a href="#"  className="social"><img src={go} className="icon" alt="Google"/></a>
+                            <a href="#"  className="social"><img src={git} className="icon" alt="Github"/></a>
                         </div>
                         <span>or use your account</span>
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
+                        <button className="In">Sign In</button>
                     </form>
                 </div>
 
@@ -72,18 +75,20 @@ export class Session extends Component {
 
                     <div className="overlay">
                         <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>To keep connected with us please login with your personal info</p>
+                        <h2 className="titleF"><strong className="Jc">JC</strong><span className="dev">dev</span></h2>
+                            <h1>Hola de nuevo!</h1>
+                            <p>Rapido inicia sesión y no pierdas tu tiempo!</p>
                             <button className="ghost" onClick={(e) => this.hideenToggle(e)} id="signIn">Sign In</button>
                         </div>
 
                         <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Enter your personal details and start journey with us</p>
+                        <h2 className="titleF"><strong className="Jc">JC</strong><span className="dev">dev</span></h2>
+                            <h1>Bienvenido!</h1>
+                            <p>Nuevo por aqui, comienza con tu registro ya!</p>
                             <button className="ghost" onClick={(e) => this.handleToggle(e)}  id="signUp">Sign Up</button>
                         </div>
                     </div>
-                    
+
                 </div>
 
             </div>

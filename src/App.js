4de89da9846d2cab0e468b97fd3635ba6b9e6412
CarlosJCdev <carlos4561059919@gmail.com>
2020-08-13 23:25:21 -0500

    Interface de Login creada

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/Style/App.css';
+import Session from './Session';
+
+function App() {
+  return (
+    <>
+     <Session/>
+    </>
+  );
+}
+
+export default App;
